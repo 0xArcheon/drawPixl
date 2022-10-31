4541643aaf38cd7e00c9b781232ac96f69580c96
0xArcheon <imamlan03@gmail.com>
2022-10-31 23:39:29 +0530

    Created initial 16X16 grid using CSS Grid

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let canvas = document.querySelector(".canvas");
-for(let i=0; i< 16; i++) {
-    let cells = document.createElement("div");
-    cells.classList.add("cells");
-    canvas.appendChild(cells);
+
+let createGrid = ()=> {
+    let canvas = document.querySelector(".canvas");
+    for(let i=0; i< 16*16; i++) {
+        let cells = document.createElement("div");
+        cells.classList.add("cells");
+        canvas.appendChild(cells);
+        }   
 }
+createGrid();
